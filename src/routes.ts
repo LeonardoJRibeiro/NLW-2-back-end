2045ff791b96f8fc59f06c265d1a99d5d5e4b2eb
Leonardo Jardim Ribeiro <leonardojr410@gmail.com>
2020-08-05 13:53:11 -0300

    finalização da api

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import db from './database/connection';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionsController
+
+const routes = express.Router();
+
+
+routes.post('/classes', classesControllers.create );
+routes.get('/classes', classesControllers.index );
+
+routes.post('/connections', connectionsController.create );
+routes.get('/connections', connectionsController.index );
+
+export default routes;
